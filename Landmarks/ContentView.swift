308b79e5224878335973596c93992f43e46d5337
Ezra Black <dev.ezrablack@gmail.com>
2021-04-23 16:15:36 -0500

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ezra Black on 4/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+                .padding()
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ezra Black on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
