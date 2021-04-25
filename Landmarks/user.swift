c0057e1ea163bb904f983a8eab4ba2a3969ada98
Ezra Black <dev.ezrablack@gmail.com>
2021-04-25 17:57:20 -0500

    Displaying User Profile

---
+++ Landmarks/user.swift
@@ -0,0 +1,26 @@
+//
+//  user.swift
+//  Landmarks
+//
+//  Created by Ezra Black on 4/25/21.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "Ezra Black")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { self.rawValue }
+    }
+}
